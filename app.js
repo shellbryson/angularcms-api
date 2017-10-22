1981b8f0b277e40d5b8a9cebdf9a90ce36d9871c
Shell Bryson <shell@shellbryson.com>
2017-10-22 16:07:45 +0100

    Adds basic database connection (MongoDB)

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-var express = require('express');
-var path = require('path');
-var mongoose = require('mongoos');
-var bodyParser = require('body-parser');
-// var config = require('express');
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const config = require('./config/database');
+
+// Connect to DB
+mongoose.connect(config.database);
+const db = mongoose.connection;
+
+// Made database connection
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to DB');
+})
+
+// Init app
+const app = express();
+const port = 3000;
+
+app.set('json spaces', 40);
+app.use(bodyParser.urlencoded({ extended: false}) );
+app.use(bodyParser.json());
+app.listen(port, function() {
+  console.log(`server running ${port}`);
+})
