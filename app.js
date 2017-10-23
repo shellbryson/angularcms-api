d15837bd4683772bd7675363e2b99dc64c7b7356
Shell Bryson <shell@shellbryson.com>
2017-10-23 11:13:52 +0100

    Adds cross-origin support so we can talk to the node api

--- app.js
+++ app.js
@@ -21,6 +21,14 @@ app.set('json spaces', 40);
 app.use(bodyParser.urlencoded({ extended: false}) );
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentuals', true);
+  next();
+})
+
 // Set Routes
 const pages = require('./routes/pages.js');
 app.use('/pages', pages);
